ef982b10847f00e293e72db0b83fcfba070d89d1
Lucas Mendes <lucasmurillomendes@hotmail.com>
2020-02-17 19:17:21 -0300

    primeiro commit

---
+++ src/ifpr_lp3/IFPR_LP3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifpr_lp3;
+
+/**
+ *
+ * @author lucas
+ */
+public class IFPR_LP3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
